6f57c25ccf4c6436728d0011aef4b6da4eaa1d0f
Ciprian Gal <ciprian@jlab.org>
2015-07-16 14:32:50 -0400

    add variables for tree and init method

--- src/mscSteppingAction.cc
+++ src/mscSteppingAction.cc
@@ -16,12 +16,34 @@ mscSteppingAction::mscSteppingAction(G4int *evN)
   //eventID pointer from the mscEventAction.cc file
   evNr=evN;
 
+  for(int i=0;i<3;i++) interactionNr.push_back(0);
+  
   /*Create root file and initialize what I want to put in it*/
   fout=new TFile("o_mscSteppingAction.root","RECREATE");
   tout=new TTree("t","Stepping action event tree");
 
-  tout->Branch("prePosX",&pre_pos_x,"prePosX/D");
-  tout->Branch("postPosX",&post_pos_x,"postPosX/D");
+  tout->Branch( "prePosX", &prePosX, "prePosX/D");
+  tout->Branch( "prePosY", &prePosY, "prePosY/D");
+  tout->Branch( "prePosZ", &prePosZ, "prePosZ/D");
+  tout->Branch("postPosX",&postPosX,"postPosX/D");
+  tout->Branch("postPosY",&postPosY,"postPosY/D");
+  tout->Branch("postPosZ",&postPosZ,"postPosZ/D");
+
+  tout->Branch( "preMomX", &preMomX, "preMomX/D");
+  tout->Branch( "preMomY", &preMomY, "preMomY/D");
+  tout->Branch( "preMomZ", &preMomZ, "preMomZ/D");
+  tout->Branch("postMomX",&postMomX,"postMomX/D");
+  tout->Branch("postMomY",&postMomY,"postMomY/D");
+  tout->Branch("postMomZ",&postMomZ,"postMomZ/D");
+
+  tout->Branch("evNr",&eventNr,"evNr/I");
+  tout->Branch("material",&material,"material/I");
+  tout->Branch("volume",&volume,"volume/I");
+  tout->Branch("pType",&pType,"pType/I");
+  tout->Branch("trackID",&trackID,"trackID/I");
+  tout->Branch("parentID",&parentID,"parentID/I");
+  tout->Branch("intNr",&intNr,"intNr/I");
+
   
 }
 
@@ -37,22 +59,28 @@ mscSteppingAction::~mscSteppingAction()
 
 void mscSteppingAction::UserSteppingAction(const G4Step* theStep)
 {
-
-
+  static G4int currentEvent(-1);
+  
   G4Track*              theTrack     = theStep->GetTrack();
   G4ParticleDefinition* particleType = theTrack->GetDefinition();
   G4StepPoint*          thePrePoint  = theStep->GetPreStepPoint();
   G4StepPoint*          thePostPoint = theStep->GetPostStepPoint();
-  //G4VPhysicalVolume*    thePostPV    = thePostPoint->GetPhysicalVolume();
+  G4VPhysicalVolume*    thePostPV    = thePostPoint->GetPhysicalVolume();
   G4String              particleName = theTrack->GetDefinition()->GetParticleName();
   
   //get material
   G4Material* theMaterial = theTrack->GetMaterial();
 
+  InitVar();
 
+  if(currentEvent!=eventNr){ //new event
+    interactionNr.clear();
+  }
 
   if(theMaterial){
+    
     if(theMaterial->GetName().compare("detectorMat")==0){
+      
       G4cout<<" In  detector " << *evNr<<" "
 	    <<theTrack->GetTrackID()<<" "<< theTrack->GetParentID()<< " "
 	    <<thePrePoint->GetPosition().getX()<<" "
@@ -62,7 +90,7 @@ void mscSteppingAction::UserSteppingAction(const G4Step* theStep)
 	    <<thePrePoint->GetTotalEnergy()<<" "
 	    <<particleType->GetPDGEncoding()<<G4endl;    
     }
-    if(theMaterial->GetName().compare("PBA")==0){
+    else if(theMaterial->GetName().compare("PBA")==0){
       G4cout<<" In  radiator " << *evNr<<" "
 	    <<theTrack->GetTrackID()<<" "<< theTrack->GetParentID()<< " "
 	    <<thePrePoint->GetPosition().getX()<<" "
@@ -72,16 +100,41 @@ void mscSteppingAction::UserSteppingAction(const G4Step* theStep)
 	    <<thePrePoint->GetTotalEnergy()<<" "
 	    <<particleType->GetPDGEncoding()<<G4endl;   
     }
+    
   }
 
   
   /*fill tree*/ 
 
-  pre_pos_x = thePrePoint->GetPosition().getX();
-  post_pos_x = thePostPoint->GetPosition().getX();
+  prePosX = thePrePoint->GetPosition().getX();
+  postPosX = thePostPoint->GetPosition().getX();
 
   tout->Fill();
 
+}
+
+void mscSteppingAction::InitVar(){
+   prePosX = -999;
+   prePosY = -999;
+   prePosZ = -999;
+  postPosX = -999;
+  postPosY = -999;
+  postPosZ = -999;
+
+   preMomX = -999;
+   preMomY = -999;
+   preMomZ = -999;
+  postMomX = -999;
+  postMomY = -999;
+  postMomZ = -999;
+
+  eventNr = -999;
+  material = -999;
+  volume = -999;
+  pType = -999;
+  trackID = -999;
+  parentID = -999;
+  intNr = -999;
 }
 
 
