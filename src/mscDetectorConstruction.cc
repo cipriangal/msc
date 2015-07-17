06385862f321b0196f386a6a95e2add18f2cede3
Ciprian Gal <ciprian@jlab.org>
2015-07-17 15:12:22 -0400

    radiator thickness as a multiple of radiation length. proper size for detector and positions

--- src/mscDetectorConstruction.cc
+++ src/mscDetectorConstruction.cc
@@ -39,8 +39,9 @@ mscDetectorConstruction::mscDetectorConstruction()
   fMessenger 
     = new G4GenericMessenger(this, "/msc/det/", "Detector construction control");
 
-  radiatorThickness = 2.*cm;  
-  // Define /msc/det/setRadiatorWidth command
+  G4double PbRadiationLength = 0.5612 * cm;
+  radiatorThickness = 0.5 * PbRadiationLength;  
+  // Define /msc/det/setRadiatorThickness command
   G4GenericMessenger::Command& setRadiatorThicknessCmd
     = fMessenger->DeclareMethod("setRadiatorThickness", 
                                  &mscDetectorConstruction::SetRadiatorThickness, 
@@ -102,13 +103,14 @@ void mscDetectorConstruction::DefineMaterials()
 
 G4VPhysicalVolume* mscDetectorConstruction::DefineVolumes()
 {
+
   // Geometry parameters
-  G4double detectorThickness = 1.*cm;
+  G4double detectorThickness = 1.*mm;
   G4double SizeX  = 100.*cm;
   G4double SizeY  =  20.*cm;
 
   G4double worldSizeXY = 200 * cm;
-  G4double worldSizeZ  =  20 * cm; 
+  G4double worldSizeZ  =  50 * cm; 
   
   // Get materials
   G4Material* defaultMaterial = G4Material::GetMaterial("Galactic");
@@ -175,7 +177,7 @@ G4VPhysicalVolume* mscDetectorConstruction::DefineVolumes()
  
   G4VSolid* Detector1Solid 
     = new G4Box("Detector1",  // its name
-		SizeX/2, SizeY/2, detectorThickness); // its size
+		SizeX/2, SizeY/2, detectorThickness/2); // its size
   
   G4LogicalVolume* detector1Logical
     = new G4LogicalVolume(
@@ -185,7 +187,7 @@ G4VPhysicalVolume* mscDetectorConstruction::DefineVolumes()
   
   new G4PVPlacement(
 		    0,                // no rotation
-		    G4ThreeVector(0., 0., radiatorThickness), 
+		    G4ThreeVector(0., 0., (radiatorThickness+detectorThickness)/2+1*mm), 
 		    detector1Logical,          // its logical volume                    
 		    "Detector1",    // its name
 		    worldLV,          // its mother  volume
@@ -197,7 +199,7 @@ G4VPhysicalVolume* mscDetectorConstruction::DefineVolumes()
   /*add new detector 3 cm behind detector 1*/
   G4VSolid* Detector2Solid 
     = new G4Box("Detector2",  // its name
-		SizeX/2, SizeY/2, detectorThickness); // its size
+		SizeX/2, SizeY/2, detectorThickness/2); // its size
   
   G4LogicalVolume* detector2Logical
     = new G4LogicalVolume(
@@ -207,7 +209,7 @@ G4VPhysicalVolume* mscDetectorConstruction::DefineVolumes()
   
   new G4PVPlacement(
 		    0,                // no rotation
-		    G4ThreeVector(0., 0., (radiatorThickness) + 3*cm), 
+		    G4ThreeVector(0., 0., (radiatorThickness+detectorThickness)/2 + 3.1*cm), 
 		    detector2Logical,          // its logical volume                    
 		    "Detector2",    // its name
 		    worldLV,          // its mother  volume
